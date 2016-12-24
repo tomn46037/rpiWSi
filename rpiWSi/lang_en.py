2b80f2494c22c8045914f9f3e91b6895dec7ba71
Tom Neville <tomn@sneaky.net>
2016-12-24 00:01:01 -0500

    Changes to get things up and going.

---
+++ rpiWSi/lang_en.py
@@ -0,0 +1,51 @@
+#==============================================================
+#next arrays are the names of days and months on you language
+days_of_week={1:u"Mon",
+              2:u"Tue",
+              3:u"Wed",
+              4:u"Thur",
+              5:u"Fri",
+              6:u"Sat",
+              0:u"Sun"
+          }
+
+months={
+        1 :u"January",
+        2 :u"February",
+        3 :u"March",
+        4 :u"April",
+        5 :u"May",
+        6 :u"June",
+        7 :u"July",
+        8 :u"August",
+        9 :u"September",
+        10:u"October",
+        11:u"November",
+        12:u"December",
+        }
+
+short_months={
+        1 :u"Jan",
+        2 :u"Feb",
+        3 :u"Mar",
+        4 :u"Apr",
+        5 :u"May",
+        6 :u"Jun",
+        7 :u"Jul",
+        8 :u"Aug",
+        9 :u"Sep",
+        10:u"Oct",
+        11:u"Nov",
+        12:u"Dec",
+        }
+
+#other localizing strings:
+str_tomorrow=u"Tomorrow"
+str_pressure=u"Pressure"
+str_humidity=u"Humidity"
+str_wind=u"Wind"
+str_pressure_units_line_1=u"in"
+str_pressure_units_line_2=u""
+str_pressure_units_line_3=u"Hg"
+str_wind_speed=u"mph"
+#==============================================================
